9ea1bef298a0e9bff234bc8906a183127ce97c9f
shaan Yadav <shaanyworkspace@gmail.com>
2024-12-15 21:53:03 +0530

    Initial commit

---
+++ src/main/java/com/shelfmaster/ShelfMasterApplication.java
@@ -0,0 +1,13 @@
+package com.shelfmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelfMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shelfmaster/ShelfMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelfmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
